eb04cb1cfd7e0a9e3cbe32b0a523b4a9100bb498
Irak Darek Ortiz Soqui <37721867+pirer48@users.noreply.github.com>
2021-11-01 12:24:35 -0700

    Update settings.php

--- crud/inc/settings.php
+++ crud/inc/settings.php
@@ -3,6 +3,7 @@ $servername = "ec2-54-210-226-209.compute-1.amazonaws.com";
 $username = "msxdmzqpabnbos";
 $password = "6aae8d639554200dd4e9ae9bef836c42d3991f22932a19e13fc5c9ad7fa0839f";
 $dbname = "dump";
+$port=5432;
 
 function validar(){
     session_start();
